d4e0685540941ca82915a3c69d1e341856087d8f
unknown <maneehrumbaa1@gmail.com>
2024-05-08 07:57:38 +0545

    project initialized

---
+++ README.md
@@ -0,0 +1,5 @@
+## GIT_HUB_PROGRAM
+this is the best project in the world to learn git
+
+
+Author : Manish Rumba
---
+++ main.py
@@ -0,0 +1,10 @@
+import backend
+
+def main():
+    user = input("Enter name: ")
+    greet = backend.hello_world(user)
+    print(greet)
+
+
+
+main()
